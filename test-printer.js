cb449c6a57a336cfba582060f657e2a91af953e8
angoularaphael <germainraphaelangoulaonambele@gmail.com>
2026-05-13 14:26:20 +0200

    wqqwyghtfgvuyje00d2rwaqzweastqr

---
+++ test-printer.js
@@ -0,0 +1,16 @@
+const net = require('net');
+const socket = new net.Socket();
+socket.setTimeout(5000);
+socket.once('connect', () => {
+  console.log('✅ Connected!');
+  socket.destroy();
+});
+socket.once('timeout', () => {
+  console.log('⏱️ Timeout');
+  socket.destroy();
+});
+socket.once('error', (err) => {
+  console.log('❌ Error:', err.message);
+  socket.destroy();
+});
+socket.connect(9100, '192.168.1.26');
